73b171f3a0f6e78d5d1e4d1afaec564c4af13243
Oluwafemi Akinde <oluwafemiakinde@gmail.com>
2024-09-16 01:17:16 +0100

    feat: implement links extractor

---
+++ crawler/crawler.go
@@ -0,0 +1,48 @@
+package crawler
+
+import (
+	"io"
+	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func Fetch(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+// extract links from html content
+func ExtractLinks(body string) ([]string, error) {
+	doc, err := html.Parse(strings.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	var links []string
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					links = append(links, a.Val)
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+	return links, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module web-crawler
 
 go 1.22.5
+
+require golang.org/x/net v0.29.0 // indirect
